416a003a781e6d2142448455bb82cd6f77b717eb
Oussama <saoudio@mcmaster.ca>
2020-04-04 23:45:54 -0400

    Created API for ConvexHullBuilder and added Point and Stack files

---
+++ src/disasterarea/builder/ConvexHullBuilder.java
@@ -0,0 +1,25 @@
+package disasterarea.builder;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+
+public class ConvexHullBuilder {
+	private Stack<Point> pointStack;
+	
+	private Point nextToTop() {}
+	
+	private int exch(Point p1, Point p2) {}
+	
+	private int distanceSq(Point p1,Point p2) {}
+	
+	private int orientation(Point p1, Point p2, Point p3) {}
+	
+	private class PointComparator implements Comparator<Point>{
+
+		@Override
+		public int compare(Point arg0, Point arg1) {
+			// TODO Auto-generated method stub
+			return 0;
+		}}
+	private void convexHull(ArrayList<Point> points, int n) {}
+}
---
+++ src/disasterarea/builder/Point.java
@@ -0,0 +1,7 @@
+package disasterarea.builder;
+
+public class Point {
+	private int id;
+	private double lat;
+	private double lon;
+}
