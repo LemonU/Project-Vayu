ff48157127f5b8f1e078c344417c5a5e213e8ed9
Oussama <saoudio@mcmaster.ca>
2020-04-06 19:37:49 -0400

    Completed documentation for the KDTree dataType

--- src/disasterarea/builder/Point.java
+++ src/disasterarea/builder/Point.java
@@ -1,10 +1,50 @@
 package disasterarea.builder;
 
+/**
+ * Point data type which contains x and y positions which represent
+ * latitude and longitude respectively. Each point corresponds to a 
+ * disaster point, which is identified with the id field.
+ * @author OussamaSaoudi
+ *
+ */
 public class Point {
 	private int id;
 	private double lat;
 	private double lon;
 	
-	public double x() {}
-	public double y() {}
+	/**
+	 * Constructs Point from the id, latitude, and longitude, which 
+	 * corresponds to a DisasterPoint object
+	 * @param id Id of the corresponding event
+	 * @param lat latitude of the event
+	 * @param lon longitude of the event
+	 */
+	public Point(int id, double lat, double lon) {
+		this.id = id;
+		this.lat = lat;
+		this.lon = lon;
+	}
+	
+	/**
+	 * Returns the x position, ie the latitude of the Point
+	 * @return Double latitude of the point
+	 */
+	public double x() {
+		return this.lat;
+	}
+	/**
+	 * Returns the y position, ie the longitude of the Point
+	 * @return Double longitude of the point
+	 */
+	public double y() {
+		return this.lon;
+	}
+	/**
+	 * Returns the id of the DisasterPoint object corresponding to the
+	 * Point
+	 * @return Integer id of the point
+	 */
+	public int id() {
+		return this.id;
+	}
 }
