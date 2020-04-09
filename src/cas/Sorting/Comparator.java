97edbe5f2b173a8d8c1eceda7bc26f480dda7d03
Womfidence <dms6013@gmail.com>
2020-04-08 21:39:45 -0400

    Created branch Sorting

---
+++ src/cas/Sorting/Comparator.java
@@ -0,0 +1,14 @@
+package cas.Sorting;
+
+public class Comparator {
+	
+	public int compare(Object obj1, Object obj2) {
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/cas/Sorting/Quicksort.java
@@ -0,0 +1,10 @@
+package cas.Sorting;
+
+public class Quicksort {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
